521511d37e79ae3de4a6c001db0ed6ac51e5effb
Sushma T N <sushmatn@in.ibm.com>
2020-02-05 17:15:25 +0530

    Calculation test project

---
+++ src/main/java/com/sushma/application/Calculations.java
@@ -0,0 +1,23 @@
+package com.sushma.application;
+public class Calculations {
+	public int addition(int firstValue, int secondValue) {
+		return firstValue + secondValue;
+	}
+
+	public int multiply(int i, int j) {
+		return i * j;
+	}
+
+	public double division(double i, double j) {
+		if (j == 0) {
+			throw new ArithmeticException();
+		}
+		return i / j;
+
+	}
+
+	 public int substraction(int i, int j) {
+		return i-j;
+	}
+
+}
